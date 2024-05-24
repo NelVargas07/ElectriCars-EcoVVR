386acebdbe3eede884dc50217b00453d410a4c1e
NelVargas07 <vargascalderonnelson@gmail.com>
2024-05-24 17:13:42 -0600

    liato

--- StoreProcedure.sql
+++ StoreProcedure.sql
@@ -1,153 +1,160 @@
---USE ElectriCars
---go
+USE ElectriCars
+go
 
---CREATE PROCEDURE sp_listar_clientes(
---   @ID int = NULL,
---   @nombre NVARCHAR(50) = NULL,
---   @apellido NVARCHAR(50) = NULL,
---   @direccion NVARCHAR(300) = NULL,
---   @telefono NVARCHAR(100) = NULL,
---   @correo NVARCHAR(100) = NULL,
---   @contrasenia NVARCHAR(100) = NULL)
---AS
---BEGIN
---	SELECT 
---	[ID], [nombre], [apellido], 
---	[direccion], [telefono], [correo], 
---	[contrasenia], [activo]
---   FROM Cliente
---   WHERE 
---		activo = 1
---       AND (@ID IS NULL OR ID = @ID)
---       AND (@nombre IS NULL OR nombre = @nombre)
---       AND (@apellido IS NULL OR apellido = @apellido)
---       AND (@direccion IS NULL OR direccion = @direccion)
---       AND (@telefono IS NULL OR telefono = @telefono)
---       AND (@correo IS NULL OR correo = @correo)
---       AND (@contrasenia IS NULL OR contrasenia = @contrasenia)
---END;
+CREATE PROCEDURE sp_listar_clientes(
+  @ID int = NULL,
+  @nombre NVARCHAR(50) = NULL,
+  @apellido NVARCHAR(50) = NULL,
+  @direccion NVARCHAR(300) = NULL,
+  @telefono NVARCHAR(100) = NULL,
+  @correo NVARCHAR(100) = NULL,
+  @contrasenia NVARCHAR(100) = NULL)
+AS
+BEGIN
+	SELECT 
+	[ID], [nombre], [apellido], 
+	[direccion], [telefono], [correo], 
+	[contrasenia], [activo]
+  FROM Cliente
+  WHERE 
+		activo = 1
+      AND (@ID IS NULL OR ID = @ID)
+      AND (@nombre IS NULL OR nombre = @nombre)
+      AND (@apellido IS NULL OR apellido = @apellido)
+      AND (@direccion IS NULL OR direccion = @direccion)
+      AND (@telefono IS NULL OR telefono = @telefono)
+      AND (@correo IS NULL OR correo = @correo)
+      AND (@contrasenia IS NULL OR contrasenia = @contrasenia)
+END;
+go
 
+CREATE VIEW vw_listar_clientes
+as
+select ID, nombre,apellido,direccion,telefono,correo,contrasenia from Cliente
+where activo = 1
+go
 
---CREATE VIEW vw_listar_clientes
---as
---select ID, nombre,apellido,direccion,telefono,correo,contrasenia from Cliente
---where activo = 1
-
---CREATE PROCEDURE sp_inicio_sesion_cliente
---	@correo varchar(100),
---	@contrasenia varchar(100)
---AS
---BEGIN
+CREATE PROCEDURE sp_inicio_sesion_cliente
+	@correo varchar(100),
+	@contrasenia varchar(100)
+AS
+BEGIN
 	
---	IF EXISTS(SELECT 1 FROM Cliente WHERE correo = @correo AND contrasenia = @contrasenia)
---		BEGIN
+	IF EXISTS(SELECT 1 FROM Cliente WHERE correo = @correo AND contrasenia = @contrasenia)
+		BEGIN
 		
---			RETURN 1
---		END
---	RETURN 0
---END
+			RETURN 1
+		END
+	RETURN 0
+END
+go
 
---CREATE VIEW vw_listar_vehiculos
---as
---select
---V.ID, TV.tipoVehiculo,
---V.modelo, V.marca, V.anio, 
---V.precio, V.stock, V.imagen
---from Vehiculo V
---inner join TipoVehiculo TV on TV.ID = V.TipoVehiculoID
---where activo = 1 and v.stock >= 1
+CREATE VIEW vw_listar_vehiculos
+as
+select
+V.ID, TV.tipoVehiculo,
+V.modelo, V.marca, V.anio, 
+V.precio, V.stock, V.imagen
+from Vehiculo V
+inner join TipoVehiculo TV on TV.ID = V.TipoVehiculoID
+where activo = 1 and v.stock >= 1
+go
 
---CREATE PROCEDURE sp_insertar_Cliente
---	@nombre varchar(50),
---	@apellido varchar(50),
---	@direccion varchar(100),
---	@telefono varchar(10),
---	@correo varchar(100),
---	@contrasenia varchar(100)
---as
---begin
---	BEGIN TRANSACTION 
---	IF EXISTS( SELECT 1 FROM Cliente WHERE @correo = correo)
---	BEGIN
---		ROLLBACK TRANSACTION;
---		RETURN 0;
---	END
+CREATE PROCEDURE sp_insertar_Cliente
+	@nombre varchar(50),
+	@apellido varchar(50),
+	@direccion varchar(100),
+	@telefono varchar(10),
+	@correo varchar(100),
+	@contrasenia varchar(100)
+as
+begin
+	BEGIN TRANSACTION 
+	IF EXISTS( SELECT 1 FROM Cliente WHERE @correo = correo)
+	BEGIN
+		ROLLBACK TRANSACTION;
+		RETURN 0;
+	END
 	
---	INSERT INTO Cliente (nombre, apellido, direccion, telefono, correo, contrasenia,activo)
---	VALUES (@nombre, @apellido, @direccion, @telefono, @correo, @contrasenia,1)
+	INSERT INTO Cliente (nombre, apellido, direccion, telefono, correo, contrasenia,activo)
+	VALUES (@nombre, @apellido, @direccion, @telefono, @correo, @contrasenia,1)
 	
---	IF @@ERROR <> 0
---	BEGIN
---		ROLLBACK;
---		RETURN 0;
---	END
---	COMMIT TRANSACTION;
---	RETURN 1;
+	IF @@ERROR <> 0
+	BEGIN
+		ROLLBACK;
+		RETURN 0;
+	END
+	COMMIT TRANSACTION;
+	RETURN 1;
 
---end
+end
+go
 
---CREATE VIEW vw_listar_piezas
---as
---select P.ID, CP.nombreCategoria, P.nombre, P.descripcion, P.precio, P.stock from Pieza P
---		join categoriaPieza CP ON CP.ID = P.CategoriaPiezaID
---where P.activo = 1
+CREATE VIEW vw_listar_piezas
+as
+select P.ID, CP.nombreCategoria, P.nombre, P.descripcion, P.precio, P.stock from Pieza P
+		join categoriaPieza CP ON CP.ID = P.CategoriaPiezaID
+where P.activo = 1
+go
 
---CREATE PROCEDURE sp_Delete_Pieza
---    @ID INT
---AS
---BEGIN
---	BEGIN TRANSACTION 
---	IF NOT EXISTS ( SELECT 1 FROM Pieza WHERE ID = @ID)
---	BEGIN
---		ROLLBACK TRANSACTION;
---		RETURN 0;
---	END
---     UPDATE Pieza 
---	 SET activo = 0
---	 WHERE ID = @ID;
+CREATE PROCEDURE sp_Delete_Pieza
+   @ID INT
+AS
+BEGIN
+	BEGIN TRANSACTION 
+	IF NOT EXISTS ( SELECT 1 FROM Pieza WHERE ID = @ID)
+	BEGIN
+		ROLLBACK TRANSACTION;
+		RETURN 0;
+	END
+    UPDATE Pieza 
+	 SET activo = 0
+	 WHERE ID = @ID;
 
---	 IF @@ERROR <> 0
---	BEGIN
---		ROLLBACK;
---		RETURN 0;
---	END
+	 IF @@ERROR <> 0
+	BEGIN
+		ROLLBACK;
+		RETURN 0;
+	END
 
---    COMMIT TRANSACTION;
---	RETURN 1;
+   COMMIT TRANSACTION;
+	RETURN 1;
 
---END;
+END;
+go
 
---CREATE PROCEDURE sp_actualizar_pieza
---    @ID INT,
---    @Nombre VARCHAR(50) = NULL,
---    @Descripcion VARCHAR(300) = NULL,
---    @Precio DECIMAL(18,2) = NULL,
---    @Stock INT = NULL
---AS
---BEGIN
---	BEGIN TRANSACTION 
---	IF NOT EXISTS ( SELECT 1 FROM Pieza WHERE ID = @ID)
---	BEGIN
---		ROLLBACK TRANSACTION;
---		RETURN 0;
---	END
---     UPDATE Pieza
---    SET nombre = ISNULL(@Nombre, nombre),
---        descripcion = ISNULL(@Descripcion, descripcion),
---        precio = ISNULL(@Precio, precio),
---        stock = ISNULL(@Stock, stock)
---    WHERE ID = @ID;
+CREATE PROCEDURE sp_actualizar_pieza
+   @ID INT,
+   @Nombre VARCHAR(50) = NULL,
+   @Descripcion VARCHAR(300) = NULL,
+   @Precio DECIMAL(18,2) = NULL,
+   @Stock INT = NULL
+AS
+BEGIN
+	BEGIN TRANSACTION 
+	IF NOT EXISTS ( SELECT 1 FROM Pieza WHERE ID = @ID)
+	BEGIN
+		ROLLBACK TRANSACTION;
+		RETURN 0;
+	END
+    UPDATE Pieza
+   SET nombre = ISNULL(@Nombre, nombre),
+       descripcion = ISNULL(@Descripcion, descripcion),
+       precio = ISNULL(@Precio, precio),
+       stock = ISNULL(@Stock, stock)
+   WHERE ID = @ID;
 
---	IF @@ERROR <> 0
---	BEGIN
---		ROLLBACK;
---		RETURN 0;
---	END
+	IF @@ERROR <> 0
+	BEGIN
+		ROLLBACK;
+		RETURN 0;
+	END
 
---    COMMIT TRANSACTION;
---	RETURN 1;
+   COMMIT TRANSACTION;
+	RETURN 1;
 
---END;
+END;
+go
 
 CREATE PROCEDURE sp_insertar_venta
     @correo VARCHAR(100),
@@ -228,12 +235,15 @@ BEGIN
         ROLLBACK TRANSACTION;
         RETURN 0;
     END CATCH
--- END;
+END;
+go
 
 CREATE VIEW vw_listar_categorias
 as
 select P.ID, P.nombreCategoria from CategoriaPieza P
+go
 
 CREATE VIEW vw_listar_sucursal
 AS
 SELECT ID,nombre FROM Sucursal
+go
